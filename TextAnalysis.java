e34c16fcb0479928ebbcff84189724e115538fa5
Anh Nguyen <rmn1@williams.edu>
2020-07-10 22:46:23 +0700

    20MostFrequent finished

--- TextAnalysis.java
+++ TextAnalysis.java
@@ -1,59 +1,100 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.lang.Object;
 import java.util.*;
+
+class Pair {
+    String s;
+    int occurrences;
+
+    Pair(String s, int occurrences) {
+	this.s = s;
+	this.occurrences = occurrences;
+    }
+}
 
 class TextAnalysis {
 
-    String novelContent;
-    Map<String, Integer> uniqueWords;
+    public static ArrayList<String> readFile(String fileName) {
+	ArrayList<String> content = new ArrayList<>();
 
-    public TextAnalysis(String fileName) {
-	novelContent = "";
-	getTotalNumberOfWords(fileName);
-	uniqueWords = new HashMap<>();
-    }
-
-    public int getTotalNumberOfWords(String fileName) {
-	int numsOfWords = 0;
-	StringBuilder sb = new StringBuilder();
-	
 	try {
 	    File file = new File(fileName);
 	    Scanner sc = new Scanner(file);
 
 	    while (sc.hasNext()) {
-		sb.append(sc.next());
-		sb.append(" ");
-		numsOfWords++;
+		content.add(sc.next());
 	    }
 
 	    sc.close();
-	    novelContent = sb.toString();
 	} catch (FileNotFoundException e) {
 	    System.out.println("File not found!");
 	}
 
-	return numsOfWords;
+	return content;
     }
 
-    public int getTotalUniqueWords() {
+    public static int getTotalNumberOfWords(String fileName) {
+	ArrayList<String> content = readFile(fileName);
 
-	while (novelContent.length() > 0) {
-	    String current = novelContent.substring(0, novelContent.indexOf(" "));
-	    this.uniqueWords.put(current, uniqueWords.getOrDefault(current, 0) + 1);
+	return content.size();
+    }
 
-	    if (novelContent.contains(" ")) {
-		novelContent = novelContent.substring(novelContent.indexOf(" ") + 1);
-	    } else {
-		novelContent = "";
-	    }
+    public static HashMap<String, Integer> getUniqueWords(String fileName) {
+	ArrayList<String> content = readFile(fileName);
+	HashMap<String, Integer> uniqueWords = new HashMap<>();
+
+	for (int i = 0; i < content.size(); i++) {
+	    String current = content.get(i);
+	    uniqueWords.put(current, uniqueWords.getOrDefault(current, 0) + 1);
 	}
 
-	return uniqueWords.size();
+	return uniqueWords;
+    }
+
+    public static int getTotalUniqueWords(String fileName) {
+	HashMap<String, Integer> uniqueWords = getUniqueWords(fileName);
+
+    	return uniqueWords.size();
+    }
+
+    public static Pair[] get20MostFrequentWords(String fileName) {
+	HashMap<String, Integer> uniqueWords = getUniqueWords(fileName);
+    	Comparator<String> byFrequency = (String s1, String s2) -> (uniqueWords.get(s2) - uniqueWords.get(s1));
+    	Set<String> unique = uniqueWords.keySet();
+    	List<String> uniqueWord = unique.stream().collect(Collectors.toList());
+	
+    	Collections.sort(uniqueWord, byFrequency);
+
+        Pair[] result = new Pair[20];
+
+	for (int i = 0; i < result.length; i++) {
+	    String current = uniqueWord.get(i);
+	    result[i] = new Pair(current, uniqueWords.get(current));
+	}
+
+    	return result;
     }
 
     public static void main(String[] args) {
-	TextAnalysis ta = new TextAnalysis("62441-0.txt");
-	System.out.println(ta.getTotalUniqueWords());
+	String fileName = "62441-0.txt";
+
+	// 23778
+	System.out.println("Total number of words: ");
+	System.out.println(getTotalNumberOfWords(fileName));
+
+	// 3257
+	System.out.println("Total number of unique words: ");
+	System.out.println(getTotalUniqueWords(fileName));
+
+	System.out.println("20 most frequent words: ");
+	Pair[] mostFrequent = get20MostFrequentWords(fileName);
+
+	for (int i = 0; i < mostFrequent.length; i++) {
+	    Pair current = mostFrequent[i];
+	    System.out.println(current.s + ": " + current.occurrences); 
+	}
     }
 }
